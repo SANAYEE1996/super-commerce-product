9ba038599f0772b61a15a5a4d0b99e1c62e29ecd
SANAYEE <dudtkd0219@gmail.com>
2023-05-24 20:12:14 +0900

    super-commerce-brand project open

---
+++ src/main/java/bestcommerce/brand/SuperCommerceBrandApplication.java
@@ -0,0 +1,13 @@
+package bestcommerce.brand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCommerceBrandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCommerceBrandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bestcommerce/brand/SuperCommerceBrandApplicationTests.java
@@ -0,0 +1,13 @@
+package bestcommerce.brand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCommerceBrandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
