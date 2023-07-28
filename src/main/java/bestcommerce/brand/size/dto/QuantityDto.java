b597875b56ce274bc0ce95f4ce60f67d6cb587cd
SANAYEE1996 <dudtkd0219@gmail.com>
2023-07-29 00:29:17 +0900

    hot fix

--- src/main/java/bestcommerce/brand/size/dto/QuantityDto.java
+++ src/main/java/bestcommerce/brand/size/dto/QuantityDto.java
@@ -1,5 +1,7 @@
 package bestcommerce.brand.size.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,15 @@ public class QuantityDto {
 
     private Long quantityId;
 
+    @NotNull(message = "productId must not Null")
     private Long productId;
 
+    @NotBlank(message = "quantityName must not Blank")
     private String quantityName;
 
     private int quantity;
 
-    public void setProductId(Long productId){
+    public void setProductId(@NotNull Long productId){
         this.productId = productId;
     }
 }
--- src/main/java/bestcommerce/brand/util/service/DtoValidation.java
+++ src/main/java/bestcommerce/brand/util/service/DtoValidation.java
@@ -1,29 +1,15 @@
 package bestcommerce.brand.util.service;
 
 import bestcommerce.brand.product.dto.ProductImageDto;
-import bestcommerce.brand.size.dto.QuantityDto;
 import bestcommerce.brand.size.entity.Quantity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Component
 public class DtoValidation {
-
-    public void saveQuantityCheck(List<QuantityDto> dtoList) throws RuntimeException{
-        for(QuantityDto quantityDto : dtoList){
-            if(quantityDto.getProductId() == null) {
-                throw new RuntimeException("Product Id is Null");
-            }
-            else if(!StringUtils.hasText(quantityDto.getQuantityName())) {
-                throw new RuntimeException("Quantity Name is Empty");
-            }
-        }
-    }
 
     public void sizeQuantityCheck(List<Quantity> quantityList, Set<Long> quantitySet) throws RuntimeException{
         if(quantityList.size() != quantitySet.size()){
@@ -37,21 +23,12 @@ public class DtoValidation {
         }
     }
 
-    public void validateImageTitleUpdate(List<ProductImageDto> updateList, List<Integer> newOdrList, int length) throws IOException{
-        Set<Integer> updateOdrSet = new HashSet<>();
+    public void validateImageTitleUpdate(List<ProductImageDto> updateList, Set<Integer> updateOdrSet) throws IOException{
         for(ProductImageDto productImageDto : updateList) {
             updateOdrSet.add(productImageDto.getOdr());
         }
         if(updateOdrSet.size() != updateList.size()){
             throw new IOException("Update Order parameter Error");
-        }
-        putNewOdrList(updateOdrSet, newOdrList, length);
-    }
-
-    private void putNewOdrList(Set<Integer> updateOdrSet, List<Integer> newOdrList, int length){
-        for(int i = 0; i < length; i++){
-            if(updateOdrSet.contains(i)) continue;
-            newOdrList.add(i);
         }
     }
 
