1fc88ac0be0ca116c9679577e170dd401e4e6be0
SANAYEE1996 <dudtkd0219@gmail.com>
2023-08-23 12:57:50 +0900

    sync service ing... not test create and passed

--- src/main/java/bestcommerce/brand/sync/SyncService.java
+++ src/main/java/bestcommerce/brand/sync/SyncService.java
@@ -1,11 +1,13 @@
 package bestcommerce.brand.sync;
 
-import bestcommerce.brand.util.ResponseDto;
+import bestcommerce.brand.product.dto.ProductDetailDto;
+import bestcommerce.brand.util.WebClientUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHeaders;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -17,19 +19,24 @@ public class SyncService {
     @Value("${item.url}")
     private String itemUrl;
 
-    public void syncToItemServiceForUpdate(){
-        WebClient webClient = WebClient.builder()
-                        .baseUrl(itemUrl)
-                        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .build()
-                        .mutate()
-                        .build();
+    private final WebClientUtil webClientUtil;
 
-        ResponseDto responseDto = webClient
+    public void syncToItemServiceForUpdate(ProductDetailDto productDetailDto) throws ParseException, RuntimeException {
+        WebClient webClient = webClientUtil.create();
+
+        String responseDto = webClient
                 .post()
-                .uri("/product/detail")
-                .bodyValue("")
+                .uri("/admin/save")
+                .bodyValue(productDetailDto)
                 .retrieve()
-                .bodyToMono(ResponseDto.class).block();
+                .bodyToMono(String.class).block();
+
+        JSONObject jsonObj = (JSONObject) new JSONParser().parse(responseDto);
+
+        String syncResponseStatus = jsonObj.get("code").toString();
+
+        if(!"200".equals(syncResponseStatus)){
+            throw new RuntimeException("동기화 실패");
+        }
     }
 }
